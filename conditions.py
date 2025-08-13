7197f78e7268f51fd386b5a454b3cd0aae14c5af
AMIT SHARMA <amit8102006sharma@gmail.com>
2025-08-13 14:30:00 +0530

    Add files via upload
    learn about conditions in python

---
+++ conditions.py
@@ -0,0 +1,40 @@
+
+'''a= int (input('enter your number '))
+if a>10:
+    print('greater than 10')
+    else
+    print('less than 10')
+print('none')
+
+marks = float(input('enter your marks '))
+if marks >= 33:
+    print('THE RESULT IS PASS ')
+else:
+    print('THE RESULT IS FAIL')
+    print('none')
+    if marks >= 60:
+        print('YOU ARE A GINEUSE ')
+print('done')
+
+# IF ELSE
+
+age =int(input('enter your age '))
+if age >= 18:
+    print('YOU ARE ADULT YOU CAN VOTE ')
+else:
+    print('YOU ARE UNDER AGE ')
+    print('THANK YOU ')
+'''
+#elif statement you caN use elif as much you want
+operand_1=int(input('enter your operand1 :  '))
+operand_2=int(input('enter your operand2:  '))
+operator=input('enter your operand (+,-,*,/):')
+
+if operator == '+':
+    print(operand_1 + operand_2)
+elif operator == '*':
+    print(operand_1 * operand_2)
+elif operator == '/':
+    print(operand_1 / operand_2)
+elif operator == '-':
+    print(operand_1 - operand_2)
